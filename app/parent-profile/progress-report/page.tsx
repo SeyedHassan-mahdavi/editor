a98fdda294895ce477d8cac831de5f87427a0a8e
SeyedHassan-mahdavi <mahdaviali19@yahoo.com>
2024-11-21 16:44:42 +0330

    update

--- app/parent-profile/progress-report/page.tsx
+++ app/parent-profile/progress-report/page.tsx
@@ -1,15 +1,14 @@
 "use client"
 import React from 'react';
-import ProgressCharts from '../../components/ProgressCharts';
-import ProgressTables from '../../components/ProgressTables';
+
 
 const ProgressReport = () => {
   return (
     <div className="min-h-screen bg-background text-text p-4">
       <h1 className="text-4xl font-bold mb-8 text-center">گزارش‌های پیشرفت کودکان</h1>
       <div className="space-y-12">
-        <ProgressCharts />
-        <ProgressTables />
+        {/* <ProgressCharts /> */}
+        {/* <ProgressTables /> */}
       </div>
     </div>
   );
